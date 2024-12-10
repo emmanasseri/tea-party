70e2fdd211349fbae8c229d856a628fefc7ed2e4
emmanasseri <enasseri02@gmail.com>
2024-12-10 00:54:05 -0600

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
-import HomePage from "./components/Homepage";
+import React, { useState } from "react";
+import { Box, Button, keyframes } from "@chakra-ui/react";
+import Header from "./components/Header"; // Assuming you have a Header component
+import FileListing from "./components/FileListing";
+import MainHeading from "./components/MainHeading";
+import PeerDisplay from "./components/PeerDisplay";
+import UploadAFile from "./components/UploadAFile";
+
+const gradientShift = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
 
 function App() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
@@ -8,25 +24,35 @@ function App() {
   const handleWalletConnect = () => {
     setIsWalletConnected(true);
   };
+  const peers = ["peer1", "peer2", "peer3", "peer4", "peer5"];
 
-  if (!isWalletConnected) {
-    // Render the "Connect Wallet" button centered on the screen
-    return (
-      <Box
-        minH="100vh"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
+  return (
+    <Box
+      minH="100vh"
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      p="6"
+      mt="20"
+      background="linear-gradient(270deg, #FBB6CE, #9F7AEA, #FBB6CE)"
+      backgroundSize="400% 400%"
+      animation={`${gradientShift} 20s ease infinite`}
+    >
+      <Header />
+      <MainHeading />
+      {isWalletConnected ? (
+        <>
+          <PeerDisplay peerList={peers} />
+          <UploadAFile />
+          <FileListing />
+        </>
+      ) : (
         <Button colorScheme="blue" size="lg" onClick={handleWalletConnect}>
           Connect Wallet
-        </Button>
-      </Box>
-    );
-  }
-
-  // If the wallet is connected, render the homepage with a gradient background
-  return <HomePage />;
+        </Button> // Show connect wallet button if not connected
+      )}
+    </Box>
+  );
 }
 
 export default App;
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -6,18 +6,6 @@ import UploadAFile from "./UploadAFile";
 import FileListing from "./FileListing";
 import PeerDisplay from "./PeerDisplay";
 
-const gradientShift = keyframes`
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-`;
-
 function HomePage() {
   const [socket, setSocket] = useState(null);
   //const [peerList, setPeerList] = useState([]);
@@ -50,9 +38,6 @@ function HomePage() {
       justifyContent="center"
       p="6"
       mt="20"
-      background="linear-gradient(270deg, #FBB6CE, #9F7AEA, #FBB6CE)"
-      backgroundSize="400% 400%"
-      animation={`${gradientShift} 20s ease infinite`}
     >
       <Header />
       <MainHeading />
