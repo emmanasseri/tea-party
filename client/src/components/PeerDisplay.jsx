aca23607ac9b243958970c0fc591a1593e5a547a
emmanasseri <enasseri02@gmail.com>
2024-12-19 20:54:52 -0600

    added labels on front end

--- client/src/components/PeerDisplay.jsx
+++ client/src/components/PeerDisplay.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Box, Image, HStack } from "@chakra-ui/react";
+import { Box, Image, HStack, VStack, Text } from "@chakra-ui/react";
 
 const PeerDisplay = ({ peerList }) => {
+  // Function to truncate the wallet address
+  const truncateAddress = (address) => {
+    return `${address.substring(0, 6)}...${address.substring(
+      address.length - 4
+    )}`;
+  };
+
   return (
     <Box
       position="relative"
@@ -19,8 +26,16 @@ const PeerDisplay = ({ peerList }) => {
         justifyContent="space-evenly"
         bottom="50%"
       >
-        {peerList.map((teacup, index) => (
-          <Image key={index} src="peer.gif" alt="Teacup" maxH={"80px"} />
+        {peerList.map((peer, index) => (
+          <VStack key={index} alignItems="center">
+            <Image src="peer.gif" alt="Teacup" maxH={"80px"} />
+            <Text fontSize="sm" color="white" fontWeight="bold">
+              {truncateAddress(peer.nodeId)}
+            </Text>
+            <Text fontSize="xs" color="white" fontWeight="bold">
+              {peer.address}
+            </Text>
+          </VStack>
         ))}
       </HStack>
     </Box>
